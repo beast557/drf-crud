8c0fac75f345f10e195d1edd68aa064f29a0bbb2
root <ujjwalbhai999@gmail.com>
2021-05-05 21:06:29 +0545

    classed based view

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -41,6 +41,20 @@ class PostDetails(APIView):
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
+    def put(self,request,id):
+        post = self.get_object(id)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,id):
+        post = self.get_object(id)
+        post.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
 
 # function based views
 @api_view(['GET'])
@@ -79,7 +93,6 @@ def post_detail_pk(request,pk):
         return Response(serializer.data)
     
     elif request.method == 'PUT':
-        # data = JSONParser().parse(request)
         serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
